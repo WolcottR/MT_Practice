f571a5d5dc0ff3f96a6b7849045a89979240a1e2
Galen Richard <richardg@new-host-3.home>
2018-11-04 12:56:28 -0500

    Further reading into chapter two

--- src/chapter_two.cpp
+++ src/chapter_two.cpp
@@ -2,14 +2,43 @@
 #include <iostream>
 #include <thread>
 
-void chapter_2_example_1_helper()
+inline int something(int & i)
 {
-    // stub until necessary
-}
+    return i;
+};
+
+struct func
+{
+    int & i;
+    func(int & i_):i(i_){}
+
+    void operator()()
+    {
+        std::cout << "Inside Thread" << std::endl;
+        for(unsigned j=0; j < 1000000; j++)
+	{
+	    i = something(i);
+	}
+    }
+};
 
 void chapter_2_example_1()
 {
     std::cout << "Starting chapter 2 example 1" << std::endl;
-
+    int some_local_state=0;
+    func my_func(some_local_state);
+    std::thread t(my_func);
+    
+    try
+    {
+        std::cout << "Outside Thread" << std::endl;
+    }
+    catch(...)
+    {
+        t.join();
+	throw;
+    }
+    t.join();
+    
     std::cout << "Ending chapter 2 example 1" << std::endl;
 }
